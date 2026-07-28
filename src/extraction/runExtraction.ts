3881b156d47b67ce47731d66901cf9dbcaa8bad1
Gestaltest2026 <20b49r@gmail.com>
2026-07-28 17:54:15 +0900

    Run extraction into canonical Applicant

--- src/extraction/runExtraction.ts
+++ src/extraction/runExtraction.ts
@@ -1,1 +1,24 @@
+import { readFile } from "fs/promises";
+import { applyExtraction } from "./applyExtraction";
+import { extractApplicant } from "./extractApplicant";
 
+async function main() {
+  const sourceText = await readFile(
+    "examples/applicant-001/source.md",
+    "utf8"
+  );
+
+  const extraction = await extractApplicant(
+    "applicant-001",
+    sourceText
+  );
+
+  const applicant = applyExtraction(extraction);
+
+  console.log(JSON.stringify(applicant, null, 2));
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
